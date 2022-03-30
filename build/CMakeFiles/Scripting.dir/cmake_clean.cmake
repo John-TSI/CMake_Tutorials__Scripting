b23e43304286f6118414f687124d7d02ce04cabf
John Roughley <jroughley@softwareinstitute.com>
2022-03-30 15:56:27 +0100

    initial commit

---
+++ build/CMakeFiles/Scripting.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scripting.dir/src/main.cpp.obj"
+  "CMakeFiles/Scripting.dir/src/main.cpp.obj.d"
+  "Scripting.exe"
+  "Scripting.exe.manifest"
+  "Scripting.pdb"
+  "libScripting.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Scripting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
